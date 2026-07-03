37443c8acc38842d4b7d424c2cc8fddcfa0d5efb
Carlo :) <carlorosso02@gmail.com>
2026-07-03 11:04:01 +0200

    fix: cleanup `src/view/__init__.py`

--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,22 +1,7 @@
-from .fetch_log import fetch_runs, extract_columns, merge_dfs
 from .figure import Figure
-from .compression_metrics import (
-    _compute_rac,
-    _extract_bdm_complexity_from_wandb,
-    _fetch_bdm_from_artifact,
-    compute_compression_metrics,
-)
-
 from .report import Report
 
 __all__ = [
-    "fetch_runs",
     "Figure",
-    "_compute_rac",
-    "_extract_bdm_complexity_from_wandb",
-    "_fetch_bdm_from_artifact",
-    "compute_compression_metrics",
     "Report",
-    "extract_columns",
-    "merge_dfs",
 ]
